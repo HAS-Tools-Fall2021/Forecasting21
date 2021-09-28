de31f41a9a2a67020934918ce33c1f0de5fa6870
Gigi Giralte <ggiralte@email.arizona.edu>
2021-09-27 20:27:54 -0700

    Bonus

--- evaluation_scripts/bonus/bonus_wk4.py
+++ evaluation_scripts/bonus/bonus_wk4.py
@@ -16,6 +16,12 @@ print("Everyone in our class:", all_names)
 # %%
 # Reads and makes a panda dataframe with all of the results from this week
 week4 = pd.read_csv('../../weekly_results/forecast_week4_results.csv')
+forecast = week4[['1week_points','2week_points']].to_numpy()
+# List of all of the names that got points this week 
+points_list = [ all_names[i] for i in range(len(forecast)) if forecast[i,0] > 0 or forecast[i,1] > 0]
+print('People getting points already:', points_list)
+# %%
+# DOES NOT FUCKING WORK
 
 # Determines who has already received points from the week1 and week2 forecasts
 points_set = set({})
@@ -40,9 +46,8 @@ print(no_points)
 flow = []
 
 for x in no_points:
-    print(x)
     temp = week4[week4['name']==x]
-    flow.append(temp['1week_forecast'].values[0])
+    flow.append(temp['2week_forecast'].values[0])
 
 name_and_flow = pd.DataFrame(list(zip(no_points, flow)), columns=['names', 'flow'])
 print(name_and_flow)
@@ -50,18 +55,7 @@ print(name_and_flow)
 sorted = name_and_flow.sort_values(by='flow', ascending=True).head(3)
 print(sorted)
 
-bonus_names = []
-
-# worst case just add the names to bonus_names from ^^
-
-# %%
-#isn't working
-
-print(sorted[['names']][:])
-
-bonus_names=sorted['names'].values
-print(bonus_names)
-
+bonus_names = ['Monique', 'Kevin', 'Andrew']
 # %%
 # Write out the bonus points
 ef.write_bonus(bonus_names, all_names, weeknum)
